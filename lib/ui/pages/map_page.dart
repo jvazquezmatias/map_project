7560af7b5cb66b1ec902e5ccd5f95527c1104a8d
jvazquezmatias <jvazquezmatias98@gmail.com>
2019-05-03 17:16:34 +0200

    Cambiados valores de size mapa (2)

--- lib/ui/pages/map_page.dart
+++ lib/ui/pages/map_page.dart
@@ -163,7 +163,7 @@ class MapUiPage extends State<MapPage> {
                       },
                       child: Icon(Icons.location_on),
                     ),
-                    SizedBox(height: size.height * 0.4),
+                    SizedBox(height: size.height * 0.35),
                     FloatingActionButton(
                       heroTag: "buttonFormatMap",
                       onPressed: () {
@@ -187,7 +187,7 @@ class MapUiPage extends State<MapPage> {
                       },
                       child: Icon(Icons.remove),
                     ),
-                    SizedBox(height: size.height * 0.01),
+                    SizedBox(height: size.height * 0.08),
                   ],
                 ),
         ));
